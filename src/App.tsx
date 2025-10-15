9dd4c3177bfcfc9de750045e191b6c85eab3c1ae
therealagt <210968687+therealagt@users.noreply.github.com>
2025-10-15 20:08:47 +0200

    basic structure

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button, Typography, Container } from '@mui/material';
+
+function App() {
+  return (
+    <Container>
+      <Typography variant="h1" component="h1" gutterBottom>
+        Just a Shop for Stuff
+      </Typography>
+      <Button variant="contained" color="primary">
+        Test Button
+      </Button>
+    </Container>
+  );
+}
+
+export default App;
